6f48795e13438ae5036c96b46dc953e7581fde5c
CryptoSnowPrince <topdirector2017@gmail.com>
2022-07-11 12:15:54 +0200

    update factory

--- contracts/DEX/GoosebumpsFactory.sol
+++ contracts/DEX/GoosebumpsFactory.sol
@@ -6,17 +6,14 @@ import './interfaces/IGoosebumpsFactory.sol';
 import './GoosebumpsPair.sol';
 
 contract GoosebumpsFactory is IGoosebumpsFactory {
-
+    bytes32 public constant INIT_CODE_PAIR_HASH = keccak256(abi.encodePacked(type(GoosebumpsPair).creationCode));
     address public override feeTo;
     address public override feeToSetter;
-    address public override migrator;
 
     mapping(address => mapping(address => address)) public override getPair;
     address[] public override allPairs;
 
-    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
-
-    constructor(address _feeToSetter) public {
+    constructor (address _feeToSetter) {
         feeToSetter = _feeToSetter;
     }
 
@@ -24,15 +21,11 @@ contract GoosebumpsFactory is IGoosebumpsFactory {
         return allPairs.length;
     }
 
-    function pairCodeHash() external pure returns (bytes32) {
-        return keccak256(type(GoosebumpsPair).creationCode);
-    }
-
     function createPair(address tokenA, address tokenB) external override returns (address pair) {
-        require(tokenA != tokenB, 'UniswapV2: IDENTICAL_ADDRESSES');
+        require(tokenA != tokenB, 'GoosebumpsFactory: IDENTICAL_ADDRESSES');
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        require(token0 != address(0), 'Goosebumps: ZERO_ADDRESS');
-        require(getPair[token0][token1] == address(0), 'Goosebumps: PAIR_EXISTS'); // single check is sufficient
+        require(token0 != address(0), 'GoosebumpsFactory: ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'GoosebumpsFactory: PAIR_EXISTS'); // single check is sufficient
         bytes memory bytecode = type(GoosebumpsPair).creationCode;
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
         assembly {
@@ -46,17 +39,12 @@ contract GoosebumpsFactory is IGoosebumpsFactory {
     }
 
     function setFeeTo(address _feeTo) external override {
-        require(msg.sender == feeToSetter, 'Goosebumps: FORBIDDEN');
+        require(msg.sender == feeToSetter, 'GoosebumpsFactory: FORBIDDEN');
         feeTo = _feeTo;
     }
 
-    function setMigrator(address _migrator) external override {
-        require(msg.sender == feeToSetter, 'Goosebumps: FORBIDDEN');
-        migrator = _migrator;
-    }
-
     function setFeeToSetter(address _feeToSetter) external override {
-        require(msg.sender == feeToSetter, 'Goosebumps: FORBIDDEN');
+        require(msg.sender == feeToSetter, 'GoosebumpsFactory: FORBIDDEN');
         feeToSetter = _feeToSetter;
     }
 }
