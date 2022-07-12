4fb723dccf1b847824784690afbba3afc5afbd71
CryptoSnowPrince <topdirector2017@gmail.com>
2022-07-12 17:05:27 +0200

    update function name with rule in IGoosebumpsCallee

--- contracts/interfaces/IGoosebumpsCallee.sol
+++ contracts/interfaces/IGoosebumpsCallee.sol
@@ -3,5 +3,5 @@
 pragma solidity 0.8.7;
 
 interface IGoosebumpsCallee {
-    function GoosebumpsCall(address sender, uint amount0, uint amount1, bytes calldata data) external;
+    function goosebumpsCall(address sender, uint amount0, uint amount1, bytes calldata data) external;
 }
